f0c2575e7fdc03f0bcdb85112cc1083866112d83
palash08 <patelpalash0808@gmail.com>
2026-07-02 18:36:31 +0530

    chore: refactor import statements

--- backend/core/database.py
+++ backend/core/database.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-from backend.core.config import settings
+from core.config import settings
 
 engine = create_engine(settings.database_url)
 
